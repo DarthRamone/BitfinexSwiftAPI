3665ce035b99c0393ab2c6c2bc7f7cb03e51d87b
Александр Мошкин <moshkin_alexander@outlook.com>
2015-11-04 23:39:12 +0300

    Initial Commit

---
+++ BitfinexSwiftAPI/BitfinexSwiftAPI.h
@@ -0,0 +1,19 @@
+//
+//  BitfinexSwiftAPI.h
+//  BitfinexSwiftAPI
+//
+//  Created by Alexander Moshkin on 04.11.15.
+//  Copyright © 2015 DarthRamone. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for BitfinexSwiftAPI.
+FOUNDATION_EXPORT double BitfinexSwiftAPIVersionNumber;
+
+//! Project version string for BitfinexSwiftAPI.
+FOUNDATION_EXPORT const unsigned char BitfinexSwiftAPIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BitfinexSwiftAPI/PublicHeader.h>
+
+
